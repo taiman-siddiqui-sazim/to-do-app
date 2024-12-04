632f461ee341d7294f3a83eabca321f22746b085
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-04 20:29:06 +0600

    feat(TD-7): Add task definition and set 'index.page' as homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/",          // Redirect the root URL "/"
+        destination: "/index.page", // To the renamed file's route "/index.page"
+        permanent: true,      // Use a 301 redirect for permanent redirection
+      },
+    ];
+  },
+};
