265084efc2f2b5b18475392befb0c3ff13b6eca1
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-05 15:41:01 +0600

    feat(TD-7): Remove unnecessary comments

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: "/",          // Redirect the root URL "/"
-        destination: "/index.page", // To the renamed file's route "/index.page"
-        permanent: true,      // Use a 301 redirect for permanent redirection
+        source: "/",          
+        destination: "/index.page", 
+        permanent: true,      
       },
     ];
   },
