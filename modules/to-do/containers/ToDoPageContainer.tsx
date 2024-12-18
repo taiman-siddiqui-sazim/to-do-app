a85563d63c138f6b15cfb3fe7bb5b61d3451892e
Taiman Siddiqui <taiman.siddiqui@sazim.io>
2024-12-19 02:15:15 +0700

    feat(TD-22): Add feature to delete tasks from backend

--- modules/to-do/containers/ToDoPageContainer.tsx
+++ modules/to-do/containers/ToDoPageContainer.tsx
@@ -3,10 +3,10 @@ import { AddTask } from "../components/AddTask";
 import { TaskList } from "../components/TaskList";
 import { HomePageLayout } from "@/shared/layouts/HomePageLayout";
 import { ITask } from "@/shared/typedefs";
-import { deleteTaskFromApi } from "@/shared/utils/TaskApi";
 
 export const ToDoPageContainer = () => {
-  const [updatedTask, setUpdatedTask] = useState<ITask | undefined>(undefined); 
+  const [updatedTask, setUpdatedTask] = useState<ITask | undefined>(undefined);
+  const [deletedTaskId, setDeletedTaskId] = useState<number | undefined>(undefined);
 
   const handleTaskAdded = (newTask: ITask) => {
     console.log("Task added:", newTask);
@@ -20,9 +20,8 @@ export const ToDoPageContainer = () => {
 
   const handleTaskDeleted = async (taskId: number) => {
     try {
-      await deleteTaskFromApi(taskId);
       console.log("Task deleted with ID:", taskId);
-      setUpdatedTask(undefined); 
+      setDeletedTaskId(taskId); 
     } catch (error) {
       console.error("Error deleting task:", error);
     }
@@ -33,6 +32,7 @@ export const ToDoPageContainer = () => {
       <AddTask onTaskAdded={handleTaskAdded} />
       <TaskList
         updatedTask={updatedTask}
+        deletedTaskId={deletedTaskId}
         onDeleteTask={handleTaskDeleted}
         onUpdateTask={handleTaskUpdated}
       />
